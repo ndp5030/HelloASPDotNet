bd791e17b78c08b5fbd5856b3f61509f80ae39b5
Nicholas Pfleegor <ndp5030@gmail.com>
2022-05-24 18:28:49 -0400

    Git file initialized

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNet.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            ViewBag.person = name;
+            return View();
+        }
+    }
+}
